00de547bd73d461b51d6ecb1a9fb9f07ef340a30
Emerson Maki <makix222@gmail.com>
2022-01-24 20:26:10 -0600

    main now processes raw data

--- src/can_stethoscope/main.py
+++ src/can_stethoscope/main.py
@@ -3,8 +3,8 @@ from file_manager import FileManager
 
 
 def main():
-
-
+    file_manager = FileManager('civi', 'clean_civic')
+    file_manager.process_raw_filenames()
     data_processor = ProcessCanData(scope_data=file_manager.scope_data)
     data_processor.basic_stats()
 
